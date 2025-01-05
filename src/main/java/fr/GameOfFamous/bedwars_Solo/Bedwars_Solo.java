5f0b22be2070ab904e1f5c3c6e80fb12560b8cc9
GameOfFamous <lelolgamer@gmail.com>
2025-01-05 20:34:56 +0100

    Ajout de la commande /hub

--- src/main/java/fr/GameOfFamous/bedwars_Solo/Bedwars_Solo.java
+++ src/main/java/fr/GameOfFamous/bedwars_Solo/Bedwars_Solo.java
@@ -1,5 +1,6 @@
 package fr.GameOfFamous.bedwars_Solo;
 
+import fr.GameOfFamous.bedwars_Solo.Commands.CommandHub;
 import fr.GameOfFamous.bedwars_Solo.Utils.Enums.GameState;
 import fr.GameOfFamous.bedwars_Solo.Utils.Gestion.Border;
 import fr.GameOfFamous.bedwars_Solo.Utils.Manager.GameManager;
@@ -33,6 +34,8 @@ public final class Bedwars_Solo extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new onPlayerJoinEvent(), this);
         getServer().getPluginManager().registerEvents(new onPlayerQuitEvent(), this);
         getServer().getPluginManager().registerEvents(new onVillager(), this);
+
+        getCommand("hub").setExecutor(new CommandHub());
 
         GameManager.getInstance().setupTeams();
         GameManager.getInstance().setupPnj();
