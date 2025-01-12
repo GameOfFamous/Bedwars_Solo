bedd750063850cba581c25b45a22faeb15aa51c7
GameOfFamous <lelolgamer@gmail.com>
2025-01-12 11:08:34 +0100

    Ajout des zones dans les bases

--- src/main/java/fr/GameOfFamous/bedwars_Solo/Utils/Enums/Teams.java
+++ src/main/java/fr/GameOfFamous/bedwars_Solo/Utils/Enums/Teams.java
@@ -1,17 +1,14 @@
 package fr.GameOfFamous.bedwars_Solo.Utils.Enums;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.BlockFace;
 
 public enum Teams {
 
-    RED(1, "§f[§cRed§f] ","§c",Color.RED,Material.RED_WOOL, Material.RED_BED, Material.RED_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), -87.5, 69, 0.5), new Location(Bukkit.getWorld("world"), -75, 69, 0), BlockFace.WEST),
-    BLUE(2, "§f[§9Blue§f] ","§9",Color.BLUE,Material.BLUE_WOOL, Material.BLUE_BED, Material.BLUE_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), 0.5, 69, -87.5), new Location(Bukkit.getWorld("world"), 0, 69, -75), BlockFace.NORTH),
-    YELLOW(3, "§f[§eYellow§f] ","§e",Color.YELLOW,Material.YELLOW_WOOL, Material.YELLOW_BED, Material.YELLOW_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), 88.5, 69, 0.5), new Location(Bukkit.getWorld("world"), 76, 69, 0), BlockFace.EAST),
-    GREEN(4, "§f[§aGreen§f] ","§a",Color.GREEN,Material.GREEN_WOOL, Material.GREEN_BED, Material.GREEN_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), 0.5, 69, 88.5), new Location(Bukkit.getWorld("world"), 0, 69, 76), BlockFace.SOUTH);
+    RED(1, "§f[§cRed§f] ","§c",Color.RED,Material.RED_WOOL, Material.RED_BED, Material.RED_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), -87.5, 69, 0.5), new Location(Bukkit.getWorld("world"), -75, 69, 0), BlockFace.WEST, new Location(Bukkit.getWorld("world"), -70, 68, -8), new Location(Bukkit.getWorld("world"), -97, 88, 9)),
+    BLUE(2, "§f[§9Blue§f] ","§9",Color.BLUE,Material.BLUE_WOOL, Material.BLUE_BED, Material.BLUE_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), 0.5, 69, -87.5), new Location(Bukkit.getWorld("world"), 0, 69, -75), BlockFace.NORTH, new Location(Bukkit.getWorld("world"), 9, 68, -70), new Location(Bukkit.getWorld("world"), -8, 88, -97)),
+    YELLOW(3, "§f[§eYellow§f] ","§e",Color.YELLOW,Material.YELLOW_WOOL, Material.YELLOW_BED, Material.YELLOW_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), 88.5, 69, 0.5), new Location(Bukkit.getWorld("world"), 76, 69, 0), BlockFace.EAST, new Location(Bukkit.getWorld("world"), 71, 68, 9), new Location(Bukkit.getWorld("world"), 98, 88, -8)),
+    GREEN(4, "§f[§aGreen§f] ","§a",Color.GREEN,Material.GREEN_WOOL, Material.GREEN_BED, Material.GREEN_STAINED_GLASS_PANE,new Location(Bukkit.getWorld("world"), 0.5, 69, 88.5), new Location(Bukkit.getWorld("world"), 0, 69, 76), BlockFace.SOUTH, new Location(Bukkit.getWorld("world"), -8, 68, 71), new Location(Bukkit.getWorld("world"), 9, 88, 98));
 
     private final int power;
     private final String prefix;
@@ -24,8 +21,10 @@ public enum Teams {
     private final Location spawnLoc;
     private final Location bedLocation;
     private final BlockFace blockFace;
+    private final Location corner1;
+    private final Location corner2;
 
-    Teams(int power, String prefix,String color,Color colour,Material wool, Material bed,Material border, Location spawnLoc, Location bedLocation, BlockFace blockFace) {
+    Teams(int power, String prefix,String color,Color colour,Material wool, Material bed,Material border, Location spawnLoc, Location bedLocation, BlockFace blockFace, Location corner1, Location corner2) {
         this.power = power;
         this.prefix = prefix;
         this.color = color;
@@ -36,6 +35,8 @@ public enum Teams {
         this.spawnLoc = spawnLoc;
         this.bedLocation = bedLocation;
         this.blockFace = blockFace;
+        this.corner1 = corner1;
+        this.corner2 = corner2;
     }
 
     public int getPower() {
@@ -77,4 +78,27 @@ public enum Teams {
     public BlockFace getBlockFace() {
         return blockFace;
     }
+
+    public Location getCorner1() {
+        return corner1;
+    }
+
+    public Location getCorner2() {
+        return corner2;
+    }
+
+    public boolean isInZone(Location playerLocation){
+
+        if(playerLocation == null && corner1 == null && corner2 == null) return false;
+
+        double x = playerLocation.getX();
+        double y = playerLocation.getY();
+        double z = playerLocation.getZ();
+
+        return (x >= Math.min(corner1.getX(), corner2.getX()) && x <= Math.max(corner1.getX(), corner2.getX()))
+                && (y >= Math.min(corner1.getY(), corner2.getY()) && y <= Math.max(corner1.getY(), corner2.getY()))
+                && (z >= Math.min(corner1.getZ(), corner2.getZ()) && z <= Math.max(corner1.getZ(), corner2.getZ()));
+
+    }
+
 }
---
+++ src/main/java/fr/GameOfFamous/bedwars_Solo/events/onTeamUpgrade.java
@@ -0,0 +1,84 @@
+package fr.GameOfFamous.bedwars_Solo.events;
+
+import fr.GameOfFamous.bedwars_Solo.Utils.Enums.Teams;
+import fr.GameOfFamous.bedwars_Solo.Utils.Manager.GameManager;
+import fr.GameOfFamous.commons.TeamAccount;
+import fr.GameOfFamous.hellstylia_API.Utils.SendMessage;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class onTeamUpgrade implements Listener {
+
+    private final Map<UUID, Teams> playerZone = new HashMap<>();
+
+    @EventHandler
+    public void playerMove(PlayerMoveEvent e){
+        Player player = e.getPlayer();
+        Location to = e.getTo();
+        GameManager manager = GameManager.getInstance();
+        Teams teams = manager.returnPlayerTeam(player);
+        TeamAccount account = manager.teamAccounts.get(teams);
+
+        Teams currentZone = getZoneAtLocation(to, manager);
+        Teams lastZone = playerZone.get(player.getUniqueId());
+
+        if(currentZone != null && currentZone != lastZone){
+            playerZone.put(player.getUniqueId(), currentZone);
+            if(teams == currentZone){
+                player.sendMessage("Vous etes entre dans la zone : "+ currentZone.getPrefix());
+                if(account.isHealPool()){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0, true, true));
+                }
+                if(account.isSpeedPool()){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0, true, true));
+                }
+            }else{
+                TeamAccount ennemiAccount = manager.teamAccounts.get(currentZone);
+
+                if(ennemiAccount.isAlarmTrap()){
+                    player.removePotionEffect(PotionEffectType.INVISIBILITY);
+                    ennemiAccount.setAlarmTrap(false);
+                    SendMessage.sendTitle(player, "§7Trap Activée", "§eUn joueur est entré dans vôtre base");
+                }else if(ennemiAccount.isTrapBlindness()){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 0, true, true));
+                    ennemiAccount.setTrapBlindness(false);
+                    SendMessage.sendTitle(player, "§7Trap Blindness", "§eVôtre trap blindness à été activé !");
+                } else if (ennemiAccount.isTrapSlowness()) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 0, true, true));
+                    ennemiAccount.setTrapSlowness(false);
+                    SendMessage.sendTitle(player, "§7Trap Slowness", "§eVôtre trap slowness à été activé !");
+                } else if (ennemiAccount.isTrapMiningFatigue()) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 200, 0, true, true));
+                    ennemiAccount.setTrapMiningFatigue(false);
+                    SendMessage.sendTitle(player, "§7Trap Mining Fatigue", "§eVôtre trap mining fatigue à été activé !");
+                }
+            }
+
+
+        }else if(currentZone == null && lastZone != null){
+            playerZone.remove(player.getUniqueId());
+            player.sendMessage("Vous avez quitté la zone : "+ lastZone.getPrefix());
+            player.removePotionEffect(PotionEffectType.SPEED);
+            player.removePotionEffect(PotionEffectType.REGENERATION);
+        }
+    }
+
+    public static Teams getZoneAtLocation(Location location, GameManager manager){
+        for(Teams zone : manager.allTeams){
+            if(zone.isInZone(location)){
+                return zone;
+            }
+        }
+        return null;
+    }
+
+}
